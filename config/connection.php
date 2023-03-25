bc4befd029ee132cd0a9e98825ef37f12d128f4f
Zulkifli.dev <kifliaapp@gmail.com>
2023-03-25 17:54:41 +0800

    auto create db and tb

--- config/connection.php
+++ config/connection.php
@@ -3,13 +3,31 @@
   $username = "root";
   $password = "";
   $dbname = "db_project_crud";
-
-
-  try {
-    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-     echo "Connected successfully";
-  } catch (PDOException $th) {
-    echo "Connection failed: " . $th->getMessage();
+  $conn = mysqli_connect($servername, $username, $password);
+  if($conn->connect_error){
+    die("Connection failed: " . $conn->connect_error);
+  }else{
+    // create db
+    $sql = "CREATE DATABASE IF NOT EXISTS db_project_crud";
+    if ($conn->query($sql) === FALSE) {
+      echo "Database created failed";
+    }
   }
+
+  //connect to db
+  $conn = mysqli_connect($servername, $username, $password, $dbname);
+  if($conn->connect_error){
+    die("Connection failed: " . $conn->connect_error);
+  }
+
+  //create table
+  $sql = "CREATE TABLE IF NOT EXISTS users (
+    id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    nama VARCHAR(255) NOT NULL,
+    alamat VARCHAR(255) NOT NULL
+  )";
+  if ($conn->query($sql) === FALSE) {
+    echo "Table users created failed";
+  }
+ 
 ?>
