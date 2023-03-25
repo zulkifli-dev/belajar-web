3831d09a5d33f2ee0dd9800e26bb31daf21af50b
Zulkifli.dev <kifliaapp@gmail.com>
2023-03-25 16:34:51 +0800

    gunakan koneksi PDO

--- config/connection.php
+++ config/connection.php
@@ -2,17 +2,11 @@
   $servername = "localhost";
   $username = "root";
   $password = "";
-  
-  // Create connection
-  $conn = new mysqli($servername, $username, $password);
+  $dbname = "db_project_crud";
 
-  // Check connection
-  if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-  }
 
   try {
-    $conn = new PDO("mysql:host=$servername;dbname=db_project_crud", $username, $password);
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
      echo "Connected successfully";
   } catch (PDOException $th) {
